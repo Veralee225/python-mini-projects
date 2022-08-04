843002d07220517015bd75c618617db94e3c81cf
Veralee225 <Veralee225@gmail.com>
2022-08-04 17:31:28 +0100

    I added the day1 file

---
+++ 1-day.py
@@ -0,0 +1,14 @@
+#Building a band name generator
+
+#Print out the welcome message
+print("Welcome to the Band Name Generator.")
+
+# Assign the city name using the input() function and also adding a new line
+city_name = input("What is the name of the city you grew up in?\n")
+
+# Assign the pet name using input() function and also adding a new line
+
+pet_name = input("What is your pet's name?\n")
+
+#print the final result. This result is concatenated
+print("Your band name could be " + city_name + " " + pet_name)
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # python-mini-projects
+
 I'm adding the practice coding exercise from 100 Days of Code: The Complete Python Bootcamp 2022
+
+## Day 1 - I built a Band Name Generator
+
+[]
