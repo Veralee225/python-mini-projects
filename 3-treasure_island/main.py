e7130c59cda266e93b76775c586682b95140eff6
Veralee225 <Veralee225@gmail.com>
2022-08-17 11:36:31 +0100

    bug fix

--- 3-treasure_island/main.py
+++ 3-treasure_island/main.py
@@ -1,3 +1,6 @@
+from secrets import choice
+
+
 print('''
 *******************************************************************************
           |                   |                  |                     |
@@ -20,40 +23,32 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 /______/______/______/______/______/______/______/______/______/______/_____ /
 *******************************************************************************
 ''')
-print("Welcome to Treasure Island.")
-print("Your mission is to find the treasure.") 
+print("Welcome to decision making.")
+print("Your mission is to find something to eat or starve to death.") 
 
 #https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 
 #Write your code below this line 
 
-direction = input("You are standing on the rails of the third mainland bridge")
+direction = input('You\'re standing at the edge of your door, caught between leaving you house or staying in. Where do you want to go? Type "left" or "right" \n')
 
 #writing the conditional statements. New knowledge, if you don't want python to ask you
 #to assign a variable to left or any other thing listed, wrap it in quotation marks. As seen below.
 
 if direction == "left":
-    wait_or_swim = input("You have fallen into the atlantic ocean")
-elif direction == "right":
-    print("You've been crushed by a danfo. Game Over!")
+
+    level1 = input('You chose the chaotic world. There is a restuarant across the street. Type "wait" to wait for food. Type "walk" to keep moving. \n').lower()
+    if level1 == "walk":
+        choice = input('You arrived elm street unharmed. There is a building with three doors. One brown, one is ash, one black. Which color do you choose? \n').lower()
+        if choice == "brown":
+            print("It's a bar. Game Over.")
+        elif choice == "ash":
+            print("You pizza! You Win!")
+        elif choice == "black":
+            print("Your entered a gym. Game Over.")
+        else:
+            print("You chose a door that doesn't exist. Game Over.")
+    else:
+        print("You never find food. Game Over.")
 else:
-    print("You chose an option that doesn\'t exist")
-
-if wait_or_swim == "wait":
-    color = input("You arrived at ebutte metta unharmed.")
-elif wait_or_swim == "swim":
-    print("You have been eaten by a shark.\nGame Over.")
-else:
-    print("You chose an option that does not exist.")
-
-if color == "red":
-    print("You fell into a room full of fire.\nGame Over.")
-
-elif color == "green":
-    print("Congratulations, you have found the treasure!")
-
-elif color == "blue":
-    print("You fell into a ditch.\nGame Over.")
-
-else:
-    print("You chose a door that does not exist.\nGame Over.")
+    print("You never left the doooor! It was all a dream. Game Over!")
