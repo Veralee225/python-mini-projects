a077b27e750622375c8aa07187df085becb48129
Veralee225 <Veralee225@gmail.com>
2022-08-20 23:12:29 +0100

    treasure_map task

---
+++ Day-4/treasure_map.py
@@ -0,0 +1,18 @@
+#I assigned the emojis to the rows
+row1 = ["⬜️","️⬜️","️⬜️"]
+row2 = ["⬜️","⬜️","️⬜️"]
+row3 = ["⬜️️","⬜️️","⬜️️"]
+map = [row1, row2, row3]
+
+#printing the rows using f string
+print(f"{row1}\n{row2}\n{row3}")
+
+# getting input for the position
+position = input("Where do you want to put the treasure? ")
+
+horizontal = int(position[0])
+vertical = int(position[1])
+
+map[vertical - 1][horizontal - 1] = "X"
+
+print(f"{row1}\n{row2}\n{row3}")
