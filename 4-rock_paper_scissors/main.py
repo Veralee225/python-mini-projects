4ac7c3bd35f72d2abd58d18edc5826e038a7d7f1
Veralee225 <Veralee225@gmail.com>
2022-08-24 22:09:40 +0100

    bug fix

--- 4-rock_paper_scissors/main.py
+++ 4-rock_paper_scissors/main.py
@@ -1,5 +1,34 @@
 import random
 
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+game_images = [rock, paper, scissors]
+
 print("Welcome to rock paper scissors")
 
 user_action = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors")
