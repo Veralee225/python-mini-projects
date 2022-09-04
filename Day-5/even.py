ff4a5cd0817ba8c7850a8f79f1caff410cb19cab
Veralee225 <Veralee225@gmail.com>
2022-09-04 22:26:23 +0100

    Exercise 3 - fizzbuzz

---
+++ Day-5/even.py
@@ -0,0 +1,13 @@
+# Printing numbers from 1 to 99 and the total
+# range (1, 101, 2) prints 1 to 99 and range(2, 101, 2) prints 1 to 99
+
+#total = 0
+#for number in range (2, 101, 2):
+#    print(number)
+
+#Printing the even numbers
+
+sum = 0
+for i in range (2,101,2):
+    sum+=i
+print(sum)
