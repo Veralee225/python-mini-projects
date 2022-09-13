67a38f5cb17fa31407f3165c445db1882826173e
Veralee225 <Veralee225@gmail.com>
2022-09-13 04:58:47 +0100

    build: hangman_art

---
+++ Day-7/main.py
@@ -0,0 +1,23 @@
+import random
+
+word_list = ["happy", "frustration", "giving"]
+
+# randomly choose a word from the word_list and assign
+# it to a varibale called chosen_word
+chosen_word = random.choice(word_list)
+
+#used the input() function to ask the user to guess a letter, then I assigned
+# their answer to the variable guess, I made it lowercase
+guess = input("Guess a letter: ").lower()
+
+#using a Forloop to check if the user guessed the right letter
+
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")
+
+
+
+print(f"you entered {value}" )
