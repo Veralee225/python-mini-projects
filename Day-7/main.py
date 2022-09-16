371415baf5c85bed933324118b5b8cc9e7f4e0d1
Veralee225 <Veralee225@gmail.com>
2022-09-16 14:38:34 +0100

    Feat: added forloop

--- Day-7/main.py
+++ Day-7/main.py
@@ -3,24 +3,34 @@ import hangman_art import stages, logo
 from hangman_words import word_list
 from replit import clear
 
+
 word_list = ["happy", "frustration", "giving"]
 
 # randomly choose a word from the word_list and assign
 # it to a varibale called chosen_word
 chosen_word = random.choice(word_list)
 
-#used the input() function to ask the user to guess a letter, then I assigned
-# their answer to the variable guess, I made it lowercase
+display = []
+word_length = len(chosen_word)
+
+for _ in range(word_length):
+    display += "_"
+print(display)
+
+end_of_game = False
+
+while not end_of_game:
 guess = input("Guess a letter: ").lower()
 
 #using a Forloop to check if the user guessed the right letter
 
-for letter in chosen_word:
+for position in range(word_length):
+    letter = chosen_word[position]
+    print(f"Current position: {position}\n
+    Current letter: {letter}\n 
+    Guessed letter: {guess}")
     if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
-
-
-
-print(f"you entered {value}" )
+        display[position] = letter
+        
+print(f"you entered {chosen_word}." )
+print(display)
