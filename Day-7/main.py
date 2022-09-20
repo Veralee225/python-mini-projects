b40af9cd919d247c06a1cbcad4b1316a87c69142
Veralee225 <Veralee225@gmail.com>
2022-09-20 01:29:09 +0100

    Fix: removed white spaces

--- Day-7/main.py
+++ Day-7/main.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 import random
 from hangman_art import stages, logo
 from hangman_words import word_list
@@ -7,7 +6,6 @@ from hangman_words import word_list
 from replit import clear
 
 print(logo)
-
 game_is_finished = False 
 lives = len(stages) - 1
 
@@ -16,38 +14,37 @@ lives = len(stages) - 1
 
 # randomly choose a word from the word_list and assign
 # it to a varibale called chosen_word
-chosen_word = random.choice(word_list)
 
+chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
 display = []
-
 for _ in range(word_length):
     display += "_"
 
 while not game_is_finished:
-    guess = input("Guess a letter:").lower()
-
+    guess = input("Guess a letter: ").lower()
+    
 # using the clear function from replit to clear the output between guesses
     clear()
 
     if guess in display:
         print(f"You've already guessed {guess}")
 
-#using a Forloop to check if the user guessed the right letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    print(f"{' '.join(display)}")
 
-for position in range(word_length):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
-print(f"{' '.join(display)}")
-
-if guess not in chosen_word:
-    print(f"You guessed {guess}, that's not in the word. You lose a life")
-    lives -= 1
-    if lives == 0:
-        game_is_finished = True
-        print("You lose.")
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        lives -= 1
+        if lives == 0:
+            game_is_finished = True
+            print("You lose.")
+    
     if not "_" in display:
         game_is_finished = True
         print("You win.")
+    print(stages[lives])
