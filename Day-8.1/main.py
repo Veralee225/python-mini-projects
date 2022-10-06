782ded169d70a8f024fbed122df3df34a75ffc67
Veralee225 <Veralee225@gmail.com>
2022-10-06 23:43:32 +0100

    feat: added a while loop

--- Day-8.1/main.py
+++ Day-8.1/main.py
@@ -18,14 +18,30 @@ def caesar(start_text, shift_amount, cipher_direction):
     shift_amount *= -1
   for char in start_text:
 
-# call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
-
-#caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
-
-if char in alphabet:
+    # checking user input type
+    if char in alphabet:
       position = alphabet.index(char)
       new_position = position + shift_amount
       end_text += alphabet[new_position]
     else:
       end_text += char
   print(f"Here's the {cipher_direction}d result: {end_text}")
+
+# asking the user if they want to restart the program
+should_end = False
+while not should_end:
+  
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+
+#for the program to continue when the user enters a number greater than 26
+  shift = shift % 26
+
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+  restart = input("Type 'yes' if you want to go again. Otherwsie type 'no'.\n")
+
+  if restart == "no":
+    should_end = True
+    print("Goodbye")
