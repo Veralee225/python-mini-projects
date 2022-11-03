c6d0236d594dca323a36e6665ae61a02d56415e5
Veralee225 <Veralee225@gmail.com>
2022-11-03 23:03:33 +0100

    feat: wrote some shitty stuff

---
+++ Day-9.1/main.py
@@ -0,0 +1,25 @@
+from art import logo
+from replit import clear
+print(logo)
+
+user_name = int(input("What is your name"))
+
+bid_price = ("What is your bidding price? ")
+
+total_bid = {
+    "Mathew": "100",
+    "zen": "200",
+    "Cynthia": 50
+}
+
+def bid_sum(user_name, bid_price):
+  total_bid = {}
+  total_bid["user_name"] = name
+  total_bid["bid_price"] = bid
+  bid_sum.append(total_bid)
+
+def more_users():
+  if user_name:
+    clear
+  if user_name:
+    return highest_bid
