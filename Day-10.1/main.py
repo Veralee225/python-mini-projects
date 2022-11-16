83e4e76f54ee712673d76cfb55ede594dbed9708
Veralee225 <Veralee225@gmail.com>
2022-11-16 01:47:01 +0100

    feat: conditionals

--- Day-10.1/main.py
+++ Day-10.1/main.py
@@ -1,5 +1,5 @@
 from art import logo
-from replit import clear
+#from replit import clear
 
 def add(n1, n2):
     return n1 + n2
@@ -15,9 +15,19 @@ def multiply(n1, n2):
 
 dict = {}
 
-sysmbols = {
+symbols = {
     "+": add,
     "-": substract,
     "/": divide,
     "*": multiply
 }
+
+num1 = int(input("What's the first number?: "))
+num2 = int(input("What's the second number?: "))
+
+for s in symbols:
+    print(s)
+operation_symbols = input("Pick an operation from the line above: ")
+
+calculation_func = symbols[operation_symbols]
+answer = calculation_func(num1, num2)
