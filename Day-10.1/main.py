ccb49d75976129fa68cf463c717c21546b8718fe
Veralee225 <Veralee225@gmail.com>
2022-11-17 11:33:01 +0100

    feat: no idea, help!

--- Day-10.1/main.py
+++ Day-10.1/main.py
@@ -1,5 +1,5 @@
 from art import logo
-#from replit import clear
+from replit import clear
 
 def add(n1, n2):
     return n1 + n2
@@ -22,12 +22,28 @@ symbols = {
     "*": multiply
 }
 
-num1 = int(input("What's the first number?: "))
-num2 = int(input("What's the second number?: "))
+def calculator():
+    print(logo)
+    
+num1 = float(input("What's the first number?: "))
 
 for s in symbols:
     print(s)
-operation_symbols = input("Pick an operation from the line above: ")
+should_continue = True
+
+while should_continue:
+    operation_symbols = input("Pick an operation from the line above: ")
+
+num2 = float(input("What's the second number?: "))
 
 calculation_func = symbols[operation_symbols]
 answer = calculation_func(num1, num2)
+
+print(f"{num1} {operation_symbols} {num2} = {answer}")
+
+if input(f"Type 'y' to continue calculating with {answer}, or Type 'n' to start a new calculation: ") == 'y':
+    num1 = answer
+else:
+    should_continue = False
+    clear()
+    calculator()
