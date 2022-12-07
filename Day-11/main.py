69217fdea52de4005a82cdf6a9746c169ac72428
Veralee225 <Veralee225@gmail.com>
2022-12-07 01:44:22 +0100

    fix

--- Day-11/main.py
+++ Day-11/main.py
@@ -33,6 +33,9 @@
 
 #Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
 #11 is the Ace.
+import random
+from replit import clear
+from art import logo
 #cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 def deal_card():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10]
@@ -48,10 +51,19 @@ def deal_card():
 #and returns the score. 
 #Look up the sum() function to help you do this.
 
+def calculate_score(cards):
+    return sum(cards)
+
 #Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
+if sum(cards) == 21 and len(cards) == 2:
+    return 0
 
 #Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
+if 11 in cards and sum(cards) >21:
+        cards.remove(11)
+        cards.append(1)
 
+        return sum(cards)
 #Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
 
 #Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
@@ -63,3 +75,6 @@ def deal_card():
 #Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
 
 #Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
+while input("Do you want to restart the game? Type 'y' or 'n': ") == "y":
+    clear()
+    play_game()
